e9eb52dc49d91379bd7b100d0252e5b7af38991c
karthikgithubBIT <karthikdabhi78@gmail.com>
2026-01-29 22:57:28 +0530

    Fix stale redirectUrl after login

--- middleware.js
+++ middleware.js
@@ -3,19 +3,19 @@ const Review = require("./models/review");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
 
-
 module.exports.isLoggedIn = (req, res, next) => {
-    if(!req.isAuthenticated()){
-        req.session.redirectUrl = req.originalUrl;
-        req.flash("error", "you must be logged in to create listing");
-        return res.redirect("/login");
-      }
-      next();
+  if (!req.isAuthenticated()) {
+    req.session.redirectUrl = req.originalUrl;
+    req.flash("error", "You must be logged in to do that");
+    return res.redirect("/login");
+  }
+  next();
 };
 
 module.exports.saveRedirectUrl = (req, res, next) => {
-  if(req.session.redirectUrl){ 
+  if (req.session.redirectUrl) {
     res.locals.redirectUrl = req.session.redirectUrl;
+    delete req.session.redirectUrl; 
   }
   next();
 };
@@ -23,9 +23,10 @@ module.exports.saveRedirectUrl = (req, res, next) => {
 module.exports.isOwner = async (req, res, next) => {
   let { id } = req.params;
   let listing = await Listing.findById(id);
+
   if (!listing.owner.equals(res.locals.currUser._id)) {
-    req.flash("error", "you are not the owner of this listing");
-    return res.redirect(`/listings/${id}`); 
+    req.flash("error", "You are not the owner of this listing");
+    return res.redirect(`/listings/${id}`);
   }
   next();
 };
@@ -53,8 +54,9 @@ module.exports.validateReview = (req, res, next) => {
 module.exports.isReviewAuthor = async (req, res, next) => {
   let { id, reviewId } = req.params;
   let review = await Review.findById(reviewId);
+
   if (!review.author.equals(res.locals.currUser._id)) {
-    req.flash("error", "you are not the author of this review");
+    req.flash("error", "You are not the author of this review");
     return res.redirect(`/listings/${id}`);
   }
   next();
